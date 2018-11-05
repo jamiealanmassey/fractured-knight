40ec624498db2d15d65aa4d7b7f08dce98ed3bce
Unknown <jamiealanmassey@gmail.com>
2018-11-05 00:10:09 +0000

    First draft of Player files
    Added some basic Player nodes to the Player scene including an AnimatedSprite, RigidBody2D and Collider. 
    
    Also created a standard Player.gd script that is referenced by the AnimatedSprite node.

---
+++ Resources/Player/Player.gd
@@ -0,0 +1,13 @@
+# Filename: Player.gd
+# Brief: Allows the player to control and interact with the player and holds
+#        valuable information about the player state
+# Author: Jamie Massey
+# Created: 04/11/2018
+
+extends AnimatedSprite
+
+func _ready():
+	pass
+
+func _process(delta):
+	pass
