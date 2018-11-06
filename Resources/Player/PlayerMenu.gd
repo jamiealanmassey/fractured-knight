1a54ce9f5ac161e0ffe6244a359c4622108b8812
Unknown <jamiealanmassey@gmail.com>
2018-11-06 17:53:37 +0000

    Fixed Resource Issues
    Some resources weren't being stored or referenced correctly so moved material and shader out of the scene as embedded files and instead stored them in the /Shaders folder

--- Resources/Player/PlayerMenu.gd
+++ Resources/Player/PlayerMenu.gd
@@ -4,7 +4,7 @@
 # Created: 05/11/2018
 extends Area2D
 
-onready var shader = preload("res://Resources/Player/Shaders/PlayerHover.tres")
+onready var hover_mat = preload("res://Resources/Player/Shaders/hover.tres")
 onready var sprite = get_node("../AnimatedSprite")
 
 func _on_Area2D_input_event(viewport, event, shape_idx):
@@ -13,7 +13,7 @@ func _on_Area2D_input_event(viewport, event, shape_idx):
 		$CanvasLayer/Menu.show()
 
 func _on_Area2D_mouse_entered():
-	sprite.set_material(shader)
+	sprite.set_material(hover_mat)
 
 func _on_Area2D_mouse_exited():
 	sprite.set_material(null)
