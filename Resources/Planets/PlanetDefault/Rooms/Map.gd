1612228564eb43259a8b9f2a010dbedd00c4fb40
vsskkv <vsskkv@gmail.com>
2018-11-08 21:15:21 +0000

    added a camera to the player and had it follow the player

--- Resources/Planets/PlanetDefault/Rooms/Map.gd
+++ Resources/Planets/PlanetDefault/Rooms/Map.gd
@@ -1,15 +1,18 @@
+# Filename: Vikram Singh Kainth
+# Breif:
+# Author:
+# Created: 
+
 extends Node
- 
-
+## Calls function set_camera_limits to set a boundrary for the camera
 func _ready():
-	set_camera_limits
-	set_process_input(true)
+	set_camera_limits()
 
-
+##
 func set_camera_limits():
-	var map_limits = $Ground.get_used_rect()
-	var map_cell_size = $Ground.cell_size
-	$Player/Camera2D.limit_left = map_limits.position.x * map_cell_size.x
-	$Player/Camera2D.limit_right = map_limits.end.x * map_cell_size.x
-	$Player/Camera2D.limit_top = map_limits.position.y * map_cell_size.y
-	$Player/Camera2D.limit_bottom = map_limits.end.y * map_cell_size.y
+	var map_limits = $Area.get_used_rect()
+	var map_cellsize = $Area.cell_size
+	$Player/Camera2D.limit_left = map_limits.position.x * map_cellsize.x
+	$Player/Camera2D.limit_right = map_limits.end.x * map_cellsize.x
+	$Player/Camera2D.limit_top = map_limits.position.y * map_cellsize.y
+	$Player/Camera2D.limit_bottom = map_limits.end.y * map_cellsize.y
