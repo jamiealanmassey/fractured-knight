a668a6f0b4926a71852342972236f5ca4dc242a9
NathanALewis <nathanlewis258@gmail.com>
2018-11-10 16:30:18 +0000

    Added NPCs to rooms

--- Resources/Planets/PlanetDefault/NPCs/Enemy/Bandit.gd
+++ Resources/Planets/PlanetDefault/NPCs/Enemy/Bandit.gd
@@ -1,8 +1,9 @@
 extends "res://Resources/Planets/PlanetDefault/NPCs/Enemy/BaseClass.gd"
 
 var dialogue = ["Hello there!", "I am Sir Edward, a knight who owns these lands.", "If you could find my sword, then I can help you on your journey!"] 
+var threats = ["Get away!", "I'll attack if you get close!"]
 var dialogueProgression = 0;
-
+var threatCounter = 0;
 #Returns next bit of dialogue 
 func converse():
 	#TODO: add actual branching dialogue
