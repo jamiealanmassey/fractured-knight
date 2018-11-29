00d2fff721c8ef9dc468c2712eb7d5dd53105c81
NathanALewis <nathanlewis258@gmail.com>
2018-11-29 14:48:45 +0000

    Added nodes to chest for its sprite, interaction distnce and the physcial collision size

---
+++ Resources/Planets/Chest/Chest.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+
+# class member variables go here, for example:"
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	pass
+
+#Ran whenever the player interacts with the chest
+func interact():
+	pass
