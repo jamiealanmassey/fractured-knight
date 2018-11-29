c87cabc71329a775a16f3b20b2f4687a40a53ba4
NathanALewis <nathanlewis258@gmail.com>
2018-11-29 15:45:39 +0000

    Added different nodes

---
+++ Resources/UI/UI.gd
@@ -0,0 +1,25 @@
+extends CanvasLayer
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	pass
+
+func on_new_choice():
+	#called when a new choice is needed
+	#
+	pass
+
+func on_damage_recieved(damage):
+	#adjust health bar width based on the amount of damage taken
+	pass
+
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
