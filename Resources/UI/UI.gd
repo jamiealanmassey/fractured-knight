100355a19892f63209621b56469e2ee2486bb1c3
NathanALewis <nathanlewis258@gmail.com>
2018-12-02 18:09:29 +0000

    Added functions for changing UI to combat and conversation

--- Resources/UI/UI.gd
+++ Resources/UI/UI.gd
@@ -23,15 +23,27 @@ func on_new_choice():
 	#
 	pass
 
-#damage as percentage value
-func on_damage_recieved(damage):
+#sets health as percentage
+func set_heatlh(health):
 	#Update damage bar here
-	$Panel/VBoxContainer/HealthBar.rect_size.x = $Panel/VBoxContainer/HealthBar.rect_size.x - damage
+	$Panel/VBoxContainer/HealthBar.rect_size.x = health
 	#adjust health bar width based on the amount of damage taken
 	pass
 
+#Changes button texts to show combat options
+func change_to_combat():
+	$Panel/VBoxContainer/Buttons/Button1.text = "Attack"
+	$Panel/VBoxContainer/Buttons/Button2.text = "Talk"
+	$Panel/VBoxContainer/Buttons/Button3.hide()
+	$Panel/VBoxContainer/Buttons/Button4.text = "Flee"
 
-
+#Changes button texts to chat options
+func change_to_talking():
+	$Panel/VBoxContainer/Buttons/Button3.show()
+	$Panel/VBoxContainer/Buttons/Button1.text = "Option 1"
+	$Panel/VBoxContainer/Buttons/Button2.text = "Option 2"
+	$Panel/VBoxContainer/Buttons/Button3.text = "Option 3"
+	$Panel/VBoxContainer/Buttons/Button4.text = "Option 4"	
 
 #func _process(delta):
 #	# Called every frame. Delta is time since last frame.
