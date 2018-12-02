b0b9c914b061e6de45b24c8d68de3927f13cf033
NathanALewis <nathanlewis258@gmail.com>
2018-12-02 17:44:24 +0000

    Added general layout, a health bar and buttons to the UI. Added Signals to the UI node for when each button is pressed

--- Resources/UI/UI.gd
+++ Resources/UI/UI.gd
@@ -1,8 +1,17 @@
 extends CanvasLayer
 
+signal option_1_chosen
+signal option_2_chosen
+signal option_3_chosen
+signal option_4_chosen
+
+
 # class member variables go here, for example:
 # var a = 2
 # var b = "textvar"
+
+export (int) var x;
+export (int) var y;
 
 func _ready():
 	# Called when the node is added to the scene for the first time.
@@ -14,12 +23,37 @@ func on_new_choice():
 	#
 	pass
 
+#damage as percentage value
 func on_damage_recieved(damage):
+	#Update damage bar here
+	$Panel/VBoxContainer/HealthBar.rect_size.x = $Panel/VBoxContainer/HealthBar.rect_size.x - damage
 	#adjust health bar width based on the amount of damage taken
 	pass
+
+
 
 
 #func _process(delta):
 #	# Called every frame. Delta is time since last frame.
 #	# Update game logic here.
 #	pass
+
+
+func _on_Button1_pressed():
+	emit_signal("option_1_chosen")
+	pass # replace with function body
+
+
+func _on_Button2_pressed():
+	emit_signal("option_2_chosen")
+	pass # replace with function body
+
+
+func _on_Button3_pressed():
+	emit_signal("option_3_chosen")
+	pass # replace with function body
+
+
+func _on_Button4_pressed():
+	emit_signal("option_3_chosen")
+	pass # replace with function body
