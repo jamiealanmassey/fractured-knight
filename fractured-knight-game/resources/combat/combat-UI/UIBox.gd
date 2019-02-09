1da27cf02e2f67922c8ffa279b1ea683fe577657
vikram kainth <vsskkv@gmail.com>
2019-02-09 17:33:26 +0000

    Read array function made + visual updates made

--- fractured-knight-game/resources/combat/combat-UI/UIBox.gd
+++ fractured-knight-game/resources/combat/combat-UI/UIBox.gd
@@ -4,12 +4,50 @@
 #Date: 07/02/19
 extends NinePatchRect
 
-var box
-
+## uses arrow keys to move around
 func _ready():
-	pass
+	$Move1.grab_focus()
 
-func drawBox():
-	# create connection to system
-	box = add_child(Label.new())
-	
+## reads array to place elemnt from array into box
+## @param array = array of elemts that needs to be read and places into boxes
+func readArrray(array):
+	for i in range(0, array.size()):
+		if(i == 0):
+			$Move1.set_text(array[i])
+		elif(i == 1):
+			$Move2.set_text(array[i])
+		elif(i == 2):
+			$Move3.set_text(array[i])
+		elif(i == 3):
+			$Move4.set_text(array[i])
+
+## Set the postion of the cusor to the arrow so it always 
+##   points at the box where the mouse curor is
+func _on_Move1_mouse_entered():
+	$Arrow.position.x = 59
+	$Arrow.position.y = 74
+
+func _on_Move2_mouse_entered():
+	$Arrow.position.x = 59
+	$Arrow.position.y = 244
+
+func _on_Move3_mouse_entered():
+	$Arrow.position.x = 1019
+	$Arrow.position.y = 244
+
+func _on_Move4_mouse_entered():
+	$Arrow.position.x = 1019
+	$Arrow.position.y = 74
+
+## emits a signal of which button is pressed
+func _on_Move1_pressed():
+	emit_signal("Move1")
+
+func _on_Move2_pressed():
+	emit_signal("Move2")
+
+func _on_Move3_pressed():
+	emit_signal("Move3")
+
+func _on_Move4_pressed():
+	emit_signal("Move4")
--- fractured-knight-game/test/unit/test_CombatUI.gd
+++ fractured-knight-game/test/unit/test_CombatUI.gd
@@ -4,10 +4,8 @@ class TestCreationNode:
 	extends "res://addons/gut/test.gd"
 	
 	var obj = load('res://resources/combat/combat-UI/UIBox.gd')
-	var _obj = null
-	
-	func before_each():
-		_obj = obj.new()
 	
 	func test_nodeLabalCreaton():
-		pending('this test is pending')
+		obj = obj.new()
+		array = ["1", "2"]
+		
