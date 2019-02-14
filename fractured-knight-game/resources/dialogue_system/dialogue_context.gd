6e0c2e4ed2c57d6e46dbf5aa830d0e8bec08048c
Jamie Massey <jamiealanmassey@gmail.com>
2019-02-14 01:44:56 +0000

    slight changes and additional documentation

--- fractured-knight-game/resources/dialogue_system/dialogue_context.gd
+++ fractured-knight-game/resources/dialogue_system/dialogue_context.gd
@@ -13,6 +13,7 @@ var current_dialogue = null ## Current object of the active dialogue
 var dialogues = {}          ## Maps name of dialogues to their trees and roots
 var symbols = {}            ## List of symbols that exist in this context
 var processing = false      ## State of the context (a flag to show if we are currently processing a dialogue)
+var wait_branch = false     ## State indicating that the context is waiting for a branch selection
 var error = null            ## State of the context as an error (if one exists we cannot progress)
 
 signal on_context_begin   ## Called when the context has begun or has been reset
@@ -33,17 +34,19 @@ func add_dialogue_file(dialogue_name, file_name):
 	$Parser.parse(file_name)
 	dialogues[dialogue_name] = $Parser.result_full
 
+## Initiates the Dialogue Context with a new dialogue that has been stored
 func start_dialogue(dialogue_name, process = true):
 	if dialogues.has(dialogue_name):
 		current_dialogue = dialogues[dialogue_name]
 		current_node = current_dialogue['root']
 		processing = process
+		emit_signal('on_context_begin')
 	else:
 		error = 'Starting Dialogue Error: no dialogue stored with that name'
 
-## Helper function
+## Helper function to progress through the Dialogue Graph
 func evaluate_current_node():
-	if current_node == null: #|| !processing:
+	if current_node == null || !processing || wait_branch:
 		return
 	
 	match current_node.type:
@@ -52,6 +55,7 @@ func evaluate_current_node():
 			current_node = current_node.children[0]
 		DialogueNode.NodeType.Branch:
 			emit_signal('on_context_process', current_node.type, current_node.content, current_node.metadata)
+			wait_branch = true
 		DialogueNode.NodeType.Locate:
 			var location = current_node.content.stip_edges()
 			if location == 'end':
@@ -100,20 +104,24 @@ func evaluate_current_node():
 		self.conclude_dialogue()
 		return
 	
-
+## Selects the branch by index (int) and sets the current node to the selected branch
+## -> note: must figure out the index outside of this method
 func pick_branch(branch_choice):
 	if current_node.type == DialogueNode.NodeType.Branch:
 		current_node = current_node.children[branch_choice]
+		wait_branch = false
 	else:
 		error = 'Branch Choice Error: current node is not a Branch Node'
 
+## Finishes the dialogue, turning off processing state, reset current_node and current_dialogue
+## also emitting a 'finish' signal
 func conclude_dialogue():
 	processing = false
 	current_node = null
 	current_dialogue = null
 	emit_signal('on_context_finish')
 	
-
+## Evaluates symbol and returns true if it exists, false otherwise
 func evaluate_symbol(symbol_name):
 	if !symbols.has(symbol_name):
 		return false
