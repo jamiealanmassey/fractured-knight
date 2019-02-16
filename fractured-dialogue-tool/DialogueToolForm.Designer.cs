48c9e4996edeaa5b3e65cfedc883fe28a1ef2141
Jamie Massey <jamiealanmassey@gmail.com>
2019-02-16 17:11:56 +0000

    added real flow to the tool and fixed issues with maximising in general, NB: if the windows taskbar is floating left it won't work, as the Windows API will not allow me to move the form's location while it is maximised

---
+++ fractured-dialogue-tool/DialogueToolForm.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace fractured_dialogue_tool
+{
+    partial class DialogueToolForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DialogueToolForm));
+            this.Titlebar = new System.Windows.Forms.Panel();
+            this.TitlebarIcon = new System.Windows.Forms.PictureBox();
+            this.AppLabel = new System.Windows.Forms.Label();
+            this.BtnMinimise = new System.Windows.Forms.Button();
+            this.BtnMaximise = new System.Windows.Forms.Button();
+            this.BtnClose = new System.Windows.Forms.Button();
+            this.TitlebarTools = new System.Windows.Forms.Panel();
+            this.Statusbar = new System.Windows.Forms.Panel();
+            this.ToolContext = new System.Windows.Forms.TableLayoutPanel();
+            this.Titlebar.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.TitlebarIcon)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Titlebar
+            // 
+            this.Titlebar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Titlebar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.Titlebar.Controls.Add(this.TitlebarIcon);
+            this.Titlebar.Controls.Add(this.AppLabel);
+            this.Titlebar.Controls.Add(this.BtnMinimise);
+            this.Titlebar.Controls.Add(this.BtnMaximise);
+            this.Titlebar.Controls.Add(this.BtnClose);
+            this.Titlebar.Location = new System.Drawing.Point(0, 0);
+            this.Titlebar.Name = "Titlebar";
+            this.Titlebar.Size = new System.Drawing.Size(800, 31);
+            this.Titlebar.TabIndex = 3;
+            this.Titlebar.MouseDown += new System.Windows.Forms.MouseEventHandler(this.TitlebarMouseDown);
+            this.Titlebar.MouseMove += new System.Windows.Forms.MouseEventHandler(this.TitlebarMouseMove);
+            this.Titlebar.MouseUp += new System.Windows.Forms.MouseEventHandler(this.TitlebarMouseUp);
+            // 
+            // TitlebarIcon
+            // 
+            this.TitlebarIcon.Image = global::fractured_dialogue_tool.Properties.Resources.fml_ico;
+            this.TitlebarIcon.Location = new System.Drawing.Point(8, 4);
+            this.TitlebarIcon.Name = "TitlebarIcon";
+            this.TitlebarIcon.Size = new System.Drawing.Size(25, 24);
+            this.TitlebarIcon.TabIndex = 4;
+            this.TitlebarIcon.TabStop = false;
+            // 
+            // AppLabel
+            // 
+            this.AppLabel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.AppLabel.AutoSize = true;
+            this.AppLabel.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AppLabel.Location = new System.Drawing.Point(33, 8);
+            this.AppLabel.Name = "AppLabel";
+            this.AppLabel.Size = new System.Drawing.Size(140, 15);
+            this.AppLabel.TabIndex = 2;
+            this.AppLabel.Text = "Fractured Dialogue Tool";
+            // 
+            // BtnMinimise
+            // 
+            this.BtnMinimise.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.BtnMinimise.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.BtnMinimise.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnMinimise.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.BtnMinimise.FlatAppearance.BorderSize = 0;
+            this.BtnMinimise.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.BtnMinimise.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.BtnMinimise.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnMinimise.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.BtnMinimise.Image = global::fractured_dialogue_tool.Properties.Resources.icons8_minimize_window_48;
+            this.BtnMinimise.Location = new System.Drawing.Point(720, 4);
+            this.BtnMinimise.Name = "BtnMinimise";
+            this.BtnMinimise.Size = new System.Drawing.Size(22, 22);
+            this.BtnMinimise.TabIndex = 3;
+            this.BtnMinimise.UseVisualStyleBackColor = false;
+            this.BtnMinimise.Click += new System.EventHandler(this.BtnMinimiseClick);
+            // 
+            // BtnMaximise
+            // 
+            this.BtnMaximise.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.BtnMaximise.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnMaximise.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.BtnMaximise.FlatAppearance.BorderSize = 0;
+            this.BtnMaximise.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.BtnMaximise.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.BtnMaximise.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnMaximise.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.BtnMaximise.Image = global::fractured_dialogue_tool.Properties.Resources.icons8_maximize_window_48;
+            this.BtnMaximise.Location = new System.Drawing.Point(745, 4);
+            this.BtnMaximise.Margin = new System.Windows.Forms.Padding(3, 3, 6, 3);
+            this.BtnMaximise.Name = "BtnMaximise";
+            this.BtnMaximise.Size = new System.Drawing.Size(22, 22);
+            this.BtnMaximise.TabIndex = 5;
+            this.BtnMaximise.UseVisualStyleBackColor = false;
+            this.BtnMaximise.Click += new System.EventHandler(this.BtnMaximiseClick);
+            // 
+            // BtnClose
+            // 
+            this.BtnClose.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.BtnClose.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnClose.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.BtnClose.FlatAppearance.BorderSize = 0;
+            this.BtnClose.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.BtnClose.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.BtnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnClose.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.BtnClose.Image = ((System.Drawing.Image)(resources.GetObject("BtnClose.Image")));
+            this.BtnClose.Location = new System.Drawing.Point(770, 4);
+            this.BtnClose.Margin = new System.Windows.Forms.Padding(3, 3, 6, 3);
+            this.BtnClose.Name = "BtnClose";
+            this.BtnClose.Size = new System.Drawing.Size(22, 22);
+            this.BtnClose.TabIndex = 0;
+            this.BtnClose.UseVisualStyleBackColor = false;
+            this.BtnClose.Click += new System.EventHandler(this.BtnCloseClick);
+            // 
+            // TitlebarTools
+            // 
+            this.TitlebarTools.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.TitlebarTools.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.TitlebarTools.Location = new System.Drawing.Point(0, 30);
+            this.TitlebarTools.Name = "TitlebarTools";
+            this.TitlebarTools.Size = new System.Drawing.Size(800, 30);
+            this.TitlebarTools.TabIndex = 5;
+            // 
+            // Statusbar
+            // 
+            this.Statusbar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Statusbar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.Statusbar.Location = new System.Drawing.Point(0, 460);
+            this.Statusbar.Name = "Statusbar";
+            this.Statusbar.Size = new System.Drawing.Size(800, 20);
+            this.Statusbar.TabIndex = 6;
+            // 
+            // ToolContext
+            // 
+            this.ToolContext.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ToolContext.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.ToolContext.ColumnCount = 2;
+            this.ToolContext.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 80F));
+            this.ToolContext.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.ToolContext.Location = new System.Drawing.Point(0, 60);
+            this.ToolContext.Name = "ToolContext";
+            this.ToolContext.RowCount = 1;
+            this.ToolContext.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.ToolContext.Size = new System.Drawing.Size(800, 400);
+            this.ToolContext.TabIndex = 7;
+            // 
+            // DialogueToolForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.ClientSize = new System.Drawing.Size(800, 480);
+            this.Controls.Add(this.Titlebar);
+            this.Controls.Add(this.TitlebarTools);
+            this.Controls.Add(this.ToolContext);
+            this.Controls.Add(this.Statusbar);
+            this.ForeColor = System.Drawing.SystemColors.Control;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.MinimumSize = new System.Drawing.Size(640, 480);
+            this.Name = "DialogueToolForm";
+            this.Text = "Fractured Dialogue Tool";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.DialogueToolFormKeyDown);
+            this.Titlebar.ResumeLayout(false);
+            this.Titlebar.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.TitlebarIcon)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button BtnClose;
+        private System.Windows.Forms.Panel Titlebar;
+        private System.Windows.Forms.Label AppLabel;
+        private System.Windows.Forms.Button BtnMinimise;
+        private System.Windows.Forms.PictureBox TitlebarIcon;
+        private System.Windows.Forms.Button BtnMaximise;
+        private System.Windows.Forms.Panel TitlebarTools;
+        private System.Windows.Forms.Panel Statusbar;
+        private System.Windows.Forms.TableLayoutPanel ToolContext;
+    }
+}
+
---
+++ fractured-dialogue-tool/DialogueToolForm.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace fractured_dialogue_tool
+{
+    public partial class DialogueToolForm : Form
+    {
+        private GlobalMouseHandler mouseHandler = new GlobalMouseHandler();
+        private bool isDragging = false;
+        private bool isDraggingForm = false;
+
+        private Size oldFormSize = Size.Empty;
+        private Size newFormSize = Size.Empty;
+
+        private Point relative = Point.Empty;
+        private Point oldLocation = Point.Empty;
+
+        private int padding = 5;
+        private const int
+            HTLEFT = 10,
+            HTRIGHT = 11,
+            HTTOP = 12,
+            HTTOPLEFT = 13,
+            HTTOPRIGHT = 14,
+            HTBOTTOM = 15,
+            HTBOTTOMLEFT = 16,
+            HTBOTTOMRIGHT = 17;
+
+        public DialogueToolForm()
+        {
+            mouseHandler.MouseMoved += new MouseMovedEvent(GlobalMouseMoved);
+            mouseHandler.MouseDown += new MouseDownEvent(GlobalMouseDown);
+            mouseHandler.MouseUp += new MouseUpEvent(GlobalMouseUp);
+            oldFormSize = this.Size;
+            newFormSize = this.Size;
+
+            Application.AddMessageFilter(mouseHandler);
+            InitializeComponent();
+            FormBorderStyle = FormBorderStyle.None;
+            DoubleBuffered = true;
+            SetStyle(ControlStyles.ResizeRedraw, true);
+        }
+
+        private void GlobalMouseMoved()
+        {
+            if (isDraggingForm)
+            {
+                Point point = new Point(Math.Max(Cursor.Position.X - oldLocation.X, 0), Math.Max(Cursor.Position.Y - oldLocation.Y, 0));
+                newFormSize = new Size(point);
+                Size = newFormSize;
+                Cursor.Current = Cursors.SizeNWSE;
+                this.Refresh();
+            }
+
+            if (BottomRight.Contains(PointToClient(Cursor.Position)))
+                Cursor.Current = Cursors.SizeNWSE;
+        }
+
+        public void GlobalMouseDown()
+        {
+            var cursor = PointToClient(Cursor.Position);
+            if (BottomRight.Contains(cursor))
+            {
+                isDraggingForm = true;
+                oldLocation = this.Location;
+                oldFormSize = this.Size;
+                newFormSize = this.Size;
+            }
+        }
+
+        public void GlobalMouseUp()
+        {
+            Size = newFormSize;
+            isDraggingForm = false;
+        }
+
+        new Rectangle Top { get { return new Rectangle(0, 0, ClientSize.Width, padding); } }
+        new Rectangle Left { get { return new Rectangle(0, 0, padding, ClientSize.Height); } }
+        new Rectangle Bottom { get { return new Rectangle(0, ClientSize.Height - padding, ClientSize.Width, padding); } }
+        new Rectangle Right { get { return new Rectangle(ClientSize.Width - padding, 0, padding, ClientSize.Height); } }
+        
+        Rectangle TopLeft { get { return new Rectangle(0, 0, padding, padding); } }
+        Rectangle TopRight { get { return new Rectangle(ClientSize.Width - padding, 0, padding, padding); } }
+        Rectangle BottomLeft { get { return new Rectangle(0, ClientSize.Height - padding, padding, padding); } }
+        Rectangle BottomRight { get { return new Rectangle(ClientSize.Width - padding, ClientSize.Height - padding, padding, padding); } }
+
+        private void Maximise()
+        {
+            var workingArea = Screen.GetWorkingArea(this);
+            this.oldLocation = this.Location;
+            this.oldFormSize = this.Size;
+            this.MaximumSize = workingArea.Size;
+            this.WindowState = FormWindowState.Maximized;
+            this.SetBounds(workingArea.X, workingArea.Y, workingArea.Size.Width, workingArea.Size.Height);
+        }
+
+        private void Minimise()
+        {
+            var oldPosition = PointToClient(Cursor.Position);
+            this.WindowState = FormWindowState.Normal;
+            this.Location = oldLocation;
+            var difference = oldPosition.X - Location.X;
+            relative = new Point(relative.X - difference, relative.Y);
+        }
+
+        private void DialogueToolFormKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+                Close();
+        }
+
+        private void BtnMinimiseClick(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+        }
+
+        private void BtnMaximiseClick(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Maximized)
+                Minimise();
+            else
+                Maximise();
+        }
+
+        private void BtnCloseClick(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void TitlebarMouseDown(object sender, MouseEventArgs e)
+        {
+            isDragging = true;
+            relative = PointToClient(Cursor.Position);
+        }
+
+        private void TitlebarMouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDragging)
+            {
+                if (WindowState == FormWindowState.Maximized && Cursor.Position.Y > Screen.GetWorkingArea(this).Y)
+                    this.Minimise();
+                else
+                    Location = new Point(Cursor.Position.X - relative.X, Cursor.Position.Y - relative.Y);
+            }
+        }
+
+        private void TitlebarMouseUp(object sender, MouseEventArgs e)
+        {
+            isDragging = false;
+            if (WindowState != FormWindowState.Maximized && Cursor.Position.Y < Screen.GetWorkingArea(this).Y + 1)
+                Maximise();
+        }
+
+        /*private void Form_Resize(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Minimized)
+            {
+                Hide();
+            }
+        }*/
+
+        protected override void WndProc(ref Message m)
+        {
+            base.WndProc(ref m);
+
+            if (m.Msg == 0x84 || m.Msg == 0x200)
+            {
+                var cursor = PointToClient(Cursor.Position);
+
+                     if (TopLeft.Contains(cursor)) m.Result = (IntPtr)HTTOPLEFT;
+                else if (TopRight.Contains(cursor)) m.Result = (IntPtr)HTTOPRIGHT;
+                else if (BottomLeft.Contains(cursor)) m.Result = (IntPtr)HTBOTTOMLEFT;
+                else if (BottomRight.Contains(cursor)) m.Result = (IntPtr)HTBOTTOMRIGHT;
+
+                else if (Top.Contains(cursor)) m.Result = (IntPtr)HTTOP;
+                else if (Left.Contains(cursor)) m.Result = (IntPtr)HTLEFT;
+                else if (Right.Contains(cursor)) m.Result = (IntPtr)HTRIGHT;
+                else if (Bottom.Contains(cursor)) m.Result = (IntPtr)HTBOTTOM;
+            }
+        }
+
+        protected override void OnResizeBegin(EventArgs e)
+        {
+            SuspendLayout();
+            base.OnResizeBegin(e);
+        }
+
+        protected override void OnResizeEnd(EventArgs e)
+        {
+            ResumeLayout();
+            base.OnResizeEnd(e);
+        }
+    }
+
+    public delegate void MouseMovedEvent();
+    public delegate void MouseDownEvent();
+    public delegate void MouseUpEvent();
+
+    public class GlobalMouseHandler : IMessageFilter
+    {
+        private const int WM_MOUSEMOVE = 0x0200;
+        private const int WM_MOUSEDOWN = 0x0201;
+        private const int WM_MOUSEUP = 0x0202;
+
+        public event MouseMovedEvent MouseMoved;
+        public event MouseDownEvent MouseDown;
+        public event MouseUpEvent MouseUp;
+
+        #region IMessageFilter Members
+
+        public bool PreFilterMessage(ref Message m)
+        {
+            if (m.Msg == WM_MOUSEMOVE)
+                MouseMoved?.Invoke();
+            else if (m.Msg == WM_MOUSEDOWN)
+                MouseDown?.Invoke();
+            else if (m.Msg == WM_MOUSEUP)
+                MouseUp?.Invoke();
+
+            return false;
+        }
+
+        #endregion
+    }
+}
