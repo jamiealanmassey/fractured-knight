657185ea77fb2f7c289a54cde716264667261e9f
vikram <vsskkv@gmail.com>
2019-02-19 23:10:01 +0000

    All Main parts for the menu done for now, also main menu set as main scene

---
+++ fractured-knight-game/resources/main_menu/MainMenu.gd
@@ -0,0 +1,36 @@
+extends Control
+
+## Signal is sent to start the game
+func _on_Button_pressed():
+	## Start game
+	print("play")
+	pass 
+
+##  Signal is sent to load a game version
+func _on_Button2_pressed():
+	## Load game
+	print("load game")
+	pass
+
+## opns the options window
+func _on_Button3_pressed():
+	## opens the options window/pop-up
+	print("options")
+	pass
+
+## Opens the help pop-up
+func _on_Button4_pressed():
+	print("help")
+	pass # replace with function body
+
+##Opens the about us pop-up
+func _on_Button5_pressed():
+	print("About")
+	$AboutUs.show()
+	pass # replace with function body
+
+## closes the game.
+func _on_Button6_pressed():
+	print("Quit")
+	get_tree().quit()
+	pass # replace with function body
