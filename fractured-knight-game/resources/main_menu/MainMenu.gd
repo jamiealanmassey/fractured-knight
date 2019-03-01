125bc33ac579162c333fcc7f0f4b23b8aa17ebcc
vikram <vsskkv@gmail.com>
2019-03-01 22:31:10 +0000

    Buttons now emmit my own made singals.

--- fractured-knight-game/resources/main_menu/MainMenu.gd
+++ fractured-knight-game/resources/main_menu/MainMenu.gd
@@ -1,25 +1,35 @@
 extends Control
 
+signal game_start
+signal game_load
+signal game_option
+signal game_help
+
+
 ## Signal is sent to start the game
 func _on_BtnStart_pressed():
 	## Start game
+	emit_signal("game_start")
 	print("play")
 	pass 
 
 ##  Signal is sent to load a game version
 func _on_BtnLoadGame_pressed():
 	## Load game
+	emit_signal("game_load")
 	print("load game")
 	pass
 
 ## opns the options window
 func _on_BtnOptions_pressed():
 	## opens the options window/pop-up
+	emit_signal("game_option")
 	print("options")
 	pass
 
 ## Opens the help pop-up
 func _on_BtnHelp_pressed():
+	emit_signal("game_help")
 	print("help")
 	pass # replace with function body
 
