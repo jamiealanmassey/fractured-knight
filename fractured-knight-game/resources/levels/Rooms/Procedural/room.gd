003e5ed622ea9c60d71789966c998f3d4d07c5c4
vikram <vsskkv@gmail.com>
2019-03-09 23:11:36 +0000

    Fixed bug: .size not working on rooms

--- fractured-knight-game/resources/levels/Rooms/Procedural/room.gd
+++ fractured-knight-game/resources/levels/Rooms/Procedural/room.gd
@@ -8,7 +8,7 @@ var size
 ## Functuion generates a new room of the given paramters
 func make_room(pos, size):
 	position = pos
-	size = size
+	self.size = size
 	var rec = RectangleShape2D.new()
 	rec.custom_solver_bias = 0.5
 	rec.extents = size
