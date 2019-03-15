d5237a288a9d46304fbab9214fd2c2f4d84a5e4e
NathanALewis <nathanlewis258@gmail.com>
2019-03-15 20:53:29 +0000

    Added animation player for animating the spritesheet, and animated moving up

---
+++ fractured-knight-game/resources/player/Sprite.gd
@@ -0,0 +1,15 @@
+extends Sprite
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	pass
+
+#Plays a frame of animation for movement
+func play(direction):
+	# Whilst waiting, play the 0th frame. Whilst moving, play 1st to 8th frame. (For up and down at least)
+	pass
