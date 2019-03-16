5acdfad4b83d34ae4387236e589914ce2f9d70b4
NathanALewis <nathanlewis258@gmail.com>
2019-03-16 20:04:58 +0000

    Set up the signal, not connected

--- fractured-knight-game/resources/levels/level_room_switcher/Level_switcher.gd
+++ fractured-knight-game/resources/levels/level_room_switcher/Level_switcher.gd
@@ -4,13 +4,13 @@ extends Area2D
 export(String) var room_name
  
 var choosen_room
-signal room_name_val(Room_name)
+signal room_entered
 
 func _on_SwitcherBlock_body_entered(body):
 	print(room_name)
 	choosen_room = room_name
 	print(choosen_room)
-	emit_signal(room_name_val, room_name)
+	emit_signal("room_entered", choosen_room)
 	get_tree().change_scene("res://resources/levels/Rooms/Procedural/current_room.tscn")
 
 func get_room():
