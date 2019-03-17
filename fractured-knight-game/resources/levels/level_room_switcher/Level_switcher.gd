fc64f7add66272ef780dc415e1bbdb4e9d6aa5e1
NathanALewis <nathanlewis258@gmail.com>
2019-03-17 14:47:21 +0000

    Changed level switcher

--- fractured-knight-game/resources/levels/level_room_switcher/Level_switcher.gd
+++ fractured-knight-game/resources/levels/level_room_switcher/Level_switcher.gd
@@ -3,11 +3,9 @@ extends Area2D
 
 export(String, FILE, "*.tscn") var worldScene
 
-func _physics_process(delta):
 
-	var bodies = get_overlapping_bodies()
-
-	for body in bodies:
-		if (body.name == get_node("../Player").name):
-			print("next world");
-			get_tree().change_scene(worldScene)
+func _on_SwitcherBlock_body_entered(body):
+	if body.name == 'Player':
+		print("next world");
+		get_tree().change_scene(worldScene)
+	pass # replace with function body
