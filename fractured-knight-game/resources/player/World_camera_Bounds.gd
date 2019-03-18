1b50a30e09be3590082023e22895f05a5e230ea0
vikram <vsskkv@gmail.com>
2019-03-18 17:47:37 +0000

    Applied to Castle, main map and merlins tomb

--- fractured-knight-game/resources/player/World_camera_Bounds.gd
+++ fractured-knight-game/resources/player/World_camera_Bounds.gd
@@ -4,9 +4,9 @@ func _ready():
 	set_camera_position()
 
 func set_camera_position():
-	var map_limits = $Border.get_used_rect()
+	var map_limit = $Border.get_used_rect()
 	var map_cell_size = $Border.cell_size
-	$Entities/Player/Camera2D.limit_left = map_limits.position.x * map_cell_size.x
-	$Entities/Player/Camera2D.limit_right = map_limits.end.x * map_cell_size.x
-	$Entities/Player/Camera2D.limit_top = map_limits.position.y * map_cell_size.y
-	$Entities/Player/Camera2D.limit_bottom = map_limits.end.y * map_cell_size.y
+	$Entities/Player/Camera2D.limit_left = map_limit.position.x * map_cell_size.x
+	$Entities/Player/Camera2D.limit_right = map_limit.end.x * map_cell_size.x
+	$Entities/Player/Camera2D.limit_top = map_limit.position.y * map_cell_size.y
+	$Entities/Player/Camera2D.limit_bottom = map_limit.end.y * map_cell_size.y
