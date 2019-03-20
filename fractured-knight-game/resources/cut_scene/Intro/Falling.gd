b03c3d9deef4cfcacf15d97e04b95411c76261a8
Astolfo <jerryduongzz1999@gmail.com>
2019-03-20 06:18:34 +0000

    add sound for all in intro_cutscene

--- fractured-knight-game/resources/cut_scene/Intro/Falling.gd
+++ fractured-knight-game/resources/cut_scene/Intro/Falling.gd
@@ -11,6 +11,6 @@ func _ready():
 
 
 func _on_AnimatedSprite_finished_animation():
-	#emit_signal("finished_animation")
-	#get_node('/root/game_manager').switch_scene('res://resources/levels/world_map/world_tilemap/master_world_plane.tscn')
+	emit_signal("finished_animation")
+	get_node('/root/game_manager').switch_scene('res://resources/levels/world_map/world_tilemap/master_world_plane.tscn')
 	pass # replace with function body
