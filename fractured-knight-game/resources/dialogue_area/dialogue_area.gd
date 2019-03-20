87ba9e53540787aa148974b219f6e05a3a660a67
NathanALewis <nathanlewis258@gmail.com>
2019-03-20 06:21:55 +0000

    Dialogue area works, first one hit currently plays crash land dialogue

--- fractured-knight-game/resources/dialogue_area/dialogue_area.gd
+++ fractured-knight-game/resources/dialogue_area/dialogue_area.gd
@@ -18,5 +18,5 @@ func _ready():
 func _on_Area2D_body_entered(body):
 	if body.name == 'Player':
 		print("play dialogue from area");
-		get_node('/root/game_manager').start_dialogue(dialogue_name)
+		get_node('/root/LevelManager').start_dialogue(dialogue_name)
 	pass # replace with function body
