19eaa517bc5b49775c4a169ea9e17da23804b51c
NathanALewis <nathanlewis258@gmail.com>
2019-03-20 05:57:49 +0000

    Added area to play dialogue when entered

---
+++ fractured-knight-game/resources/dialogue_area/dialogue_area.gd
@@ -0,0 +1,22 @@
+extends Area2D
+
+export (String) var dialogue_name
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	pass
+
+
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
+
+
+func _on_Area2D_body_entered(body):
+	if body.name == 'Player':
+		print("play dialogue from area");
+		get_node('/root/game_manager').start_dialogue(dialogue_name)
+	pass # replace with function body
